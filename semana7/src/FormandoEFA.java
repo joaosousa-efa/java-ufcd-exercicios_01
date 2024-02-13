6f573dabe213c8997c80602581c3f449efb49051
João Sousa <joaosousanetpt@gmail.com>
2024-02-13 17:12:23 +0000

    add semana7

---
+++ semana7/src/FormandoEFA.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author João Sousa
+ */
+public class FormandoEFA extends Formando{
+    
+    private double NotaPra = 0;
+
+    @Override
+    public String toString() {
+        return "FormandoEFA {" + "Nome=" + this.getNome() + ", Numero=" + this.getNumero() + ", NotaPra=" + NotaPra + '}';
+    }
+
+    
+    
+    public double getNotaPra() {
+        return NotaPra;
+    }
+
+    public void setNotaPra(double NotaPra) {
+        this.NotaPra = NotaPra;
+    }
+    
+    @Override
+    public double CalculaNotaFinal(){
+        
+        double nota = (this.getNotaUFDC() * 0.9) + (this.getNotaPra() * 0.1);
+    
+        return nota;
+    }
+    
+}
---
+++ semana7/src/Turma.java
@@ -0,0 +1,50 @@
+
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author João Sousa
+ */
+public class Turma {
+    
+    private String NomeTurma = "";
+    private int NumeroTurma = 0;
+    private ArrayList<Formando> formandos = new ArrayList<Formando>();
+
+    public String getNomeTurma() {
+        return NomeTurma;
+    }
+
+    public void setNomeTurma(String NomeTurma) {
+        this.NomeTurma = NomeTurma;
+    }
+
+    public int getNumeroTurma() {
+        return NumeroTurma;
+    }
+
+    public void setNumeroTurma(int NumeroTurma) {
+        this.NumeroTurma = NumeroTurma;
+    }
+
+    public ArrayList<Formando> getFormandos() {
+        return formandos;
+    }
+
+    public void setFormandos(ArrayList<Formando> formandos) {
+        this.formandos = formandos;
+    }
+
+    public void AdicionaFormandos(Formando formando){
+    
+        
+        this.formandos.add(formando);
+    }
+    
+}
